b22d291e883af2341686ccd176b51f295e8429c7
Serhii Fast <fastsa85@gmail.com>
2026-09-05 15:51:23 +0200

    Implement recursive folder copy mechanism (#5)
    Implement recursive folder copy mechanism
    Add happy-path integration test

--- FolderSynchronizer/FolderSynchronizer.IntegrationTests/FolderSynchronizerServiceTests.cs
+++ FolderSynchronizer/FolderSynchronizer.IntegrationTests/FolderSynchronizerServiceTests.cs
@@ -5,6 +5,14 @@ namespace FolderSynchronizer.IntegrationTests
     [TestFixture]
     public class FolderSynchronizerServiceTests
     {
+        private FolderSynchronizationService folderSynchronizerService;
+
+        [SetUp]
+        public void Setup()
+        {
+            folderSynchronizerService = new FolderSynchronizationService();
+        }
+
         [Test]
         public void Synchronize_WhenSourceContainsFile_CopiesFileToReplica()
         {
@@ -20,8 +28,6 @@ namespace FolderSynchronizer.IntegrationTests
                 var sourceFile = Path.Combine(sourceFolder.FullName, fileName);
                 File.WriteAllText(sourceFile, fileContent);
 
-                var folderSynchronizerService = new FolderSynchronizationService();
-
                 // Act: run the synchronization
                 folderSynchronizerService.Synchronize(sourceFolder.FullName, replicaFolder.FullName);
 
@@ -30,6 +36,42 @@ namespace FolderSynchronizer.IntegrationTests
 
                 Assert.That(File.Exists(replicaFile), Is.True);
                 Assert.That(File.ReadAllText(replicaFile), Is.EqualTo(fileContent));
+            }
+            finally
+            {
+                sourceFolder.Delete(recursive: true);
+                replicaFolder.Delete(recursive: true);
+            }
+        }
+
+        [Test]
+        public void Synchronize_WhenNestedDirectory_CopiesDirectoryAndFilesToReplica()
+        {
+            // Arrange: create source and replica folders
+            var sourceFolder = Directory.CreateTempSubdirectory();
+            var replicaFolder = Directory.CreateTempSubdirectory();
+
+            try
+            {
+                var nestedFolderName = "test-subfolder";
+                var nestedFolder = Directory.CreateDirectory(Path.Combine(sourceFolder.FullName, nestedFolderName));
+
+                var nestedFileName = "test-nested.txt";
+                var nestedFileContent = "Nested file test content 123 !@#";
+                var sourceFile = Path.Combine(nestedFolder.FullName, nestedFileName);
+                File.WriteAllText(sourceFile, nestedFileContent);
+
+                // Act: run the synchronization
+                folderSynchronizerService.Synchronize(sourceFolder.FullName, replicaFolder.FullName);
+
+                // Assert: check the sub-folder and file are copied
+                var replicaNestedFolder = Path.Combine(replicaFolder.FullName, nestedFolderName);
+
+                var replicaFile = Path.Combine(replicaNestedFolder, nestedFileName);
+
+                Assert.That(Directory.Exists(replicaNestedFolder), Is.True);
+                Assert.That(File.Exists(replicaFile), Is.True);
+                Assert.That(File.ReadAllText(replicaFile), Is.EqualTo(nestedFileContent));
             }
             finally
             {
