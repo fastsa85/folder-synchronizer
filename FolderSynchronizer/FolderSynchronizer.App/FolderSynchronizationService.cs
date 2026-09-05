ca8dbd4273cefd008b928ac2c3c168eca620f16c
Serhii Fast <fastsa85@gmail.com>
2026-09-05 15:03:47 +0200

    Implement basic first synchronization (files only) (#4)
    * Implemen basic file copy  behavior
    Add  FolderSynchronizationService
    Add basic integration test verifying file copy behavior.
    
    * Run intehration tests in CI
    
    ---------
    
    Co-authored-by: Serhii Fast <Serhii.Fast@ab-inbev.com>

---
+++ FolderSynchronizer/FolderSynchronizer.App/FolderSynchronizationService.cs
@@ -0,0 +1,16 @@
+﻿namespace FolderSynchronizer.App
+{
+    internal class FolderSynchronizationService
+    {
+        internal void Synchronize(string sourceFolder, string replicaFolder)
+        {
+            foreach (var sourceFile in Directory.GetFiles(sourceFolder))
+            {
+                var fileName = Path.GetFileName(sourceFile);
+                var replicaFile = Path.Combine(replicaFolder, fileName);
+
+                File.Copy(sourceFile, replicaFile, overwrite: true);
+            }
+        }
+    }
+}
---
+++ FolderSynchronizer/FolderSynchronizer.IntegrationTests/FolderSynchronizerServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using FolderSynchronizer.App;
+
+namespace FolderSynchronizer.IntegrationTests
+{
+    [TestFixture]
+    public class FolderSynchronizerServiceTests
+    {
+        [Test]
+        public void Synchronize_WhenSourceContainsFile_CopiesFileToReplica()
+        {
+            var fileName = "test.txt";
+            var fileContent = "Test content 123 !@#";
+
+            // Arrange : create source and replica folders
+            var sourceFolder = Directory.CreateTempSubdirectory();
+            var replicaFolder = Directory.CreateTempSubdirectory();
+
+            try
+            {
+                var sourceFile = Path.Combine(sourceFolder.FullName, fileName);
+                File.WriteAllText(sourceFile, fileContent);
+
+                var folderSynchronizerService = new FolderSynchronizationService();
+
+                // Act: run the synchronization
+                folderSynchronizerService.Synchronize(sourceFolder.FullName, replicaFolder.FullName);
+
+                // Assert: check the file has been copied
+                var replicaFile = Path.Combine(replicaFolder.FullName, fileName);
+
+                Assert.That(File.Exists(replicaFile), Is.True);
+                Assert.That(File.ReadAllText(replicaFile), Is.EqualTo(fileContent));
+            }
+            finally
+            {
+                sourceFolder.Delete(recursive: true);
+                replicaFolder.Delete(recursive: true);
+            }
+        }
+    }
+}
