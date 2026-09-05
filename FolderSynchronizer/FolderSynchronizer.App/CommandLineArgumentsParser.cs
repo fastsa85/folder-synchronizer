f0386b5faa6dc35212ca736a9c19f8a19e3d03b5
Serhii Fast <fastsa85@gmail.com>
2026-09-05 14:22:09 +0200

    Handle invalid command line arguments (#3)
    * Add new unit tests for the invalid command line argumnts
    
    * Validate the command line arguments count
    
    * Validate empty arguments
    
    * Validate souce and replicae folders are not the same
    
    ---------
    
    Co-authored-by: Serhii Fast <Serhii.Fast@ab-inbev.com>

--- FolderSynchronizer/FolderSynchronizer.App/CommandLineArgumentsParser.cs
+++ FolderSynchronizer/FolderSynchronizer.App/CommandLineArgumentsParser.cs
@@ -2,12 +2,41 @@
 
 internal static class CommandLineArgumentsParser
 {
+    const int COMMAND_LINE_ARGUMENTS_COUNT = 4;
+
     internal static FolderSynchronizerOptions Parse(string[] args)
     {
+        if (args.Length != COMMAND_LINE_ARGUMENTS_COUNT)
+        {
+            throw new ArgumentException($"Exactly {COMMAND_LINE_ARGUMENTS_COUNT} command line arguments are required");
+        }
+
+        if (args.Any(x => string.IsNullOrEmpty(x)))
+        {
+            throw new ArgumentException("Command line arguments can not be empty.");
+        }
+        
+        int syncInterval;
+
+        if (!int.TryParse(args[2], out syncInterval))
+        {
+            throw new ArgumentException("Sync interval must be a valid integer.");
+        }
+
+        if (syncInterval <= 0)
+        {
+            throw new ArgumentException("Sync interval must be positive");
+        }
+
+        if (args[0].Equals(args[1], StringComparison.OrdinalIgnoreCase))
+        {
+            throw new ArgumentException("Source and replica folders must be different.");
+        }
+
         return new FolderSynchronizerOptions(
             SourceFolder: args[0],
             ReplicaFolder: args[1],
-            SyncInterval: TimeSpan.FromSeconds(int.Parse(args[2])),
+            SyncInterval: TimeSpan.FromSeconds(syncInterval),
             LogFilePath: args[3]
         );
     }
