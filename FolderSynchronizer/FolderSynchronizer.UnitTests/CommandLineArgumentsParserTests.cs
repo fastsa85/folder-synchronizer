ca65c4c3d58022f7902b90252521c0a820750c4d
Serhii Fast <Serhii.Fast@ab-inbev.com>
2026-09-05 14:05:46 +0200

    Add new unit tests for the invalid command line argumnts

--- FolderSynchronizer/FolderSynchronizer.UnitTests/CommandLineArgumentsParserTests.cs
+++ FolderSynchronizer/FolderSynchronizer.UnitTests/CommandLineArgumentsParserTests.cs
@@ -22,5 +22,107 @@ namespace FolderSynchronizer.UnitTests
             Assert.That(result.SyncInterval, Is.EqualTo(TimeSpan.FromSeconds(10)));
             Assert.That(result.LogFilePath, Is.EqualTo(@"C:\sync.log"));
         }
+
+        [Test]
+        public void Parse_WhenNotAllNumberOfArguments_ThrowsArgumentException()
+        {
+            var args = new[]
+            {
+                @"C:\Source",
+                @"C:\Replica",
+                "12"
+            };
+
+            Assert.That(() => CommandLineArgumentsParser.Parse(args), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void Parse_WhenExtraArguments_ThrowsArgumentException()
+        {
+            var args = new[]
+            {
+                @"C:\Source",
+                @"C:\Replica",
+                "10",
+                @"C:\sync.log",
+                "unexpected"
+            };
+
+            Assert.That(() => CommandLineArgumentsParser.Parse(args), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void Parse_WhenNonNumericSyncInterval_ThrowsArgumentException()
+        {
+            var args = new[]
+            {
+                @"C:\Source",
+                 @"C:\Replica",
+                "abc",              // !
+                 @"C:\sync.log"
+            };
+
+            Assert.That(() => CommandLineArgumentsParser.Parse(args), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void Parse_WhenDecimalSyncInterval_ThrowsArgumentException()
+        {
+            var args = new[]
+            {
+                @"C:\Source",
+                @"C:\Replica",
+                "12.5",         // !
+                @"C:\sync.log"
+            };
+
+            Assert.That(() => CommandLineArgumentsParser.Parse(args), Throws.ArgumentException);
+        }
+
+        [TestCase("0")]
+        [TestCase("-10")]
+        public void Parse_WhenNonPositiveSyncInterval_ThrowsArgumentException(string syncInterval)
+        {
+            var args = new[]
+            {
+                @"C:\Source",
+                @"C:\Replica",
+                syncInterval,   // !
+                @"C:\sync.log"
+            };
+
+            Assert.That(() => CommandLineArgumentsParser.Parse(args), Throws.ArgumentException);
+        }
+
+        [TestCase("", @"C:\Replica", "10", @"C:\sync.log")]
+        [TestCase(@"C:\Source", "", "10", @"C:\sync.log")]
+        [TestCase(@"C:\Source", @"C:\Replica", "", @"C:\sync.log")]
+        [TestCase(@"C:\Source", @"C:\Replica", "10", "")]
+        public void Parse_WhenArgumentIsEmpty_ThrowsArgumentException(string sourceFolder, string replicaFolder, string syncInterval, string logFilePath)
+        {
+            var args = new[]
+            {
+                sourceFolder,
+                replicaFolder,
+                syncInterval,
+                logFilePath
+            };
+
+            Assert.That(() => CommandLineArgumentsParser.Parse(args), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void Parse_WhenSourceAndReplicaFoldersAreTheSame_ThrowsArgumentException()
+        {
+            var args = new[]
+            {
+                @"C:\Data",     // !
+                 @"C:\Data",    // !
+                "10",
+                @"C:\sync.log"
+            };
+
+            Assert.That(() => CommandLineArgumentsParser.Parse(args), Throws.ArgumentException);
+        }
     }
 }
