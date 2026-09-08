eefddb706cc3e82bb667cdcbf1fe2c16ba459dd0
Serhii Fast <fastsa85@gmail.com>
2026-09-08 10:29:36 +0200

    Fix flaky test: Synchronize_WhenSourceFileWasModified_UpdatesFileInReplica (#16)
    * Set source timestamp newer
    
    * Set source timestamp newer in:
    Synchronize_WhenSourceFileWasModified_UpdatesFileInReplica
     Synchronize_WhenSourceNestedFileWasModified_UpdatesFileInReplica
    
    ---------
    
    Co-authored-by: Serhii Fast <Serhii.Fast@ab-inbev.com>

--- FolderSynchronizer/FolderSynchronizer.IntegrationTests/FolderSynchronizerServiceTests.cs
+++ FolderSynchronizer/FolderSynchronizer.IntegrationTests/FolderSynchronizerServiceTests.cs
@@ -346,6 +346,8 @@ namespace FolderSynchronizer.IntegrationTests
                 var replicaFile = Path.Combine(replicaFolder.FullName, fileName);
                 File.WriteAllText(replicaFile, replicaFileContent);
 
+                File.SetLastWriteTimeUtc(sourceFile, File.GetLastWriteTimeUtc(replicaFile).AddSeconds(1));
+
                 // Act: run the synchronization
                 folderSynchronizerService.Synchronize(sourceFolder.FullName, replicaFolder.FullName);
 
@@ -385,6 +387,8 @@ namespace FolderSynchronizer.IntegrationTests
 
                 var replicaFile = Path.Combine(replicaNestedDirectory.FullName, fileName);
                 File.WriteAllText(replicaFile, replicaFileContent);
+
+                File.SetLastWriteTimeUtc(sourceFile, File.GetLastWriteTimeUtc(replicaFile).AddSeconds(1));
 
                 // Act: run the synchronization
                 folderSynchronizerService.Synchronize(sourceFolder.FullName, replicaFolder.FullName);
